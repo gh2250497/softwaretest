0fbf2411db5ee16e17dbf43acdcf7c5046e5db9e
gh2250497 <53145515+gh2250497@users.noreply.github.com>
2021-11-24 09:57:12 +0000

    Create AllTests.java

---
+++ src/test/JUnit/AllTests.java
@@ -0,0 +1,11 @@
+package JunitTest;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MyRandomDataGeneratorTest.class})
+
+public class AllTests {
+}
